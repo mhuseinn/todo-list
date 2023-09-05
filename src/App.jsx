506b8234368ffe9f81dcf4fa7e3299004c986ea6
Muhammad Husein <m.huseinid@gmail.com>
2023-09-05 15:19:47 +0700

    updated notresponsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,103 @@
 import { useState } from "react";
-import { Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Checkbox,
+  Button,
+  TextField,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddBoxIcon from "@mui/icons-material/AddBox";
 
 function App() {
-  const [todos, setTodos] = useState(["Item 1", "Item 2"]);
+  const [todos, setTodos] = useState([]);
   const [values, setValues] = useState("");
-  const [completed, setCompleted] = useState([]);
 
   const removeItems = (id) => {
     setTodos(todos.filter((item, index) => index !== id));
   };
 
-  const completedItems = (id) => {
-    setCompleted(todos.map((item, index) => index !== id));
+  const addItems = (e) => {
+    e.preventDefault();
+    setTodos([...todos, values]);
+    setValues("");
   };
 
   return (
-    <div>
-      <Typography variant="h1">Todo List : </Typography>
-      <Typography variant="h2">Add Item : </Typography>
-      <input
-        type="text"
-        value={values}
-        onChange={(e) => setValues(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodos([...todos, values]);
-          setValues("");
+    <Box sx={{ maxWidth: "100%" }}>
+      <Stack
+        mx="15rem"
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+        sx={{
+          border: 1,
+          borderColor: "primary.main",
+          borderRadius: 2,
+          backgroundColor: "#5e59bf",
         }}
+        padding={1}
       >
-        Add
-      </button>
+        <Typography variant="h2" color="white">
+          Todo List :{" "}
+        </Typography>
+        <TextField
+          required
+          size="medium"
+          label="Add Item"
+          variant="filled"
+          sx={{
+            backgroundColor: "#ef7eb0",
+            borderRadius: 2,
+            minWidth: "400px",
+            mt: 2,
+          }}
+          value={values}
+          onChange={(e) => setValues(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          size="large"
+          onClick={addItems}
+          sx={{
+            margin: "20px",
+            minWidth: "80px",
+            color: "black",
+            backgroundColor: "#ef7eb0",
+          }}
+        >
+          <AddBoxIcon />
+        </Button>
 
-      <select name="" id="">
-        <option value="completed">Incomplete Tasks</option>
-        <option value="not-completed">Completed</option>
-      </select>
-
-      {todos.map((todosItem, index) => (
-        <>
-          <li key={index}>{todosItem}</li>
-          <input type="checkbox" onClick={() => completedItems(index)} />
-          <label htmlFor="">Mark as Complete</label>
-          <button onClick={() => removeItems(index)}>Delete</button>
-        </>
-      ))}
-    </div>
+        <List>
+          {todos.map((todosItem, index) => (
+            <ListItem
+              key={index}
+              secondaryAction={
+                <Button variant="contained" onClick={() => removeItems(index)}>
+                  {<DeleteIcon />}
+                </Button>
+              }
+              sx={{ minWidth: "400px" }}
+            >
+              <ListItemIcon>
+                <Checkbox edge="start" />
+              </ListItemIcon>
+              <ListItemText
+                sx={{ color: "white", fontWeight: "bold" }}
+                primary={todosItem}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Stack>
+    </Box>
   );
 }
 
