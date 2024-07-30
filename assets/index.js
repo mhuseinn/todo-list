ac4410d55c38623ba260703748a9f47d729abb16
Muhammad Husein <m.huseinid@gmail.com>
2024-07-30 11:57:48 +0700

    add: checkbox and mark as done

--- assets/index.js
+++ assets/index.js
@@ -2,27 +2,47 @@ const todoList = document.getElementById("list");
 const button = document.getElementById("add-button");
 const input = document.querySelector(".input-todo");
 
-// Need to add checkbox
+// Click Button to make the li element
 button.addEventListener("click", () => {
   if (input.value !== "") {
     const list = document.createElement("li");
     list.textContent = input.value;
+
+    const checked = document.createElement("input");
+    checked.type = "checkbox";
+    checked.id = "checkbox";
+    checked.name = "checkbox";
+
+    list.appendChild(checked);
     todoList.appendChild(list);
+
     input.value = "";
+
+    checked.addEventListener("change", () => {
+      if (checked.checked === true) {
+        list.remove();
+      } else {
+        return false;
+      }
+    });
   } else {
     alert("Please enter some task");
   }
 });
 
+// Click tasks to mark as done
 list.addEventListener("click", (e) => {
   const clickedItem = e.target;
-  if (clickedItem.style.textDecoration === "none") {
-    clickedItem.style.textDecoration = "line-through";
+  if (clickedItem.style.backgroundColor === "transparent") {
+    clickedItem.style.backgroundColor = "green";
+    clickedItem.style.fontWeight = "bold";
   } else {
-    clickedItem.style.textDecoration = "none";
+    clickedItem.style.backgroundColor = "transparent";
+    clickedItem.style.fontWeight = "normal";
   }
 });
 
+// Press Enter to Add Tasks
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
